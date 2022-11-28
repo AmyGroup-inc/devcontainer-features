c81bb18e55486305aa9a202cde41b5f10a86386d
xiarui.xr <xiarui.xr@antgroup.com>
2022-11-28 21:38:14 +0800

    test

--- src/kusion/install.sh
+++ src/kusion/install.sh
@@ -10,5 +10,18 @@ KUSION_URL="https://github.com/KusionStack/kusion/releases/download/${VERSION}/k
 KUSION_LOCATION="~/.kusion"
 mkdir -p ${KUSION_LOCATION}
 
+echo ${KUSION_URL}
+echo ${KUSION_LOCATION}
+
 # install Kusion pack
 wget -c  ${KUSION_URL} -qO - | tar xz -C ${KUSION_LOCATION}
+
+
+ls ${KUSION_LOCATION}
+echo $PATH
+
+location=$(which kusion)
+echo ${KUSION_LOCATION}
+
+result=$(kusion)
+echo ${result}
